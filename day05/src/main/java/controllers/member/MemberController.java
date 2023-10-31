4b03adaf0a4bfa3792259b5abc97ae25b441b8b9
Ohtaeyong <kw7946@naver.com>
2023-10-31 17:51:46 +0900

    th:text 까지

--- day05/src/main/java/controllers/member/MemberController.java
+++ day05/src/main/java/controllers/member/MemberController.java
@@ -2,6 +2,7 @@ package controllers.member;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import models.member.Member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model; // Model은 이걸로 주입
@@ -23,8 +24,28 @@ public class MemberController {
      */
 
     @GetMapping("/member/login")
-    public String login() {
+    public String login(Model model) {
+
+        model.addAttribute("userId", "user99");
+        model.addAttribute("userPw", "비밀번호");
 
         return "member/login"; // login.html
+    }
+
+    @GetMapping("/member/info")
+    public String info(Model model) {
+
+        Member member = Member.builder()
+                .userNo(1L)
+                .userId("<h1>user01</h1>")
+                .userPw("123456")
+                .userNm("사용자01")
+                .email("user01@test.org")
+                .mobile("010-0000-0000")
+                .build();
+
+        model.addAttribute("member", member);
+
+        return "member/info";
     }
 }
